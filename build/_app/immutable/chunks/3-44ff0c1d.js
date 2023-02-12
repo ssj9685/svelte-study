54ce0a36d56454ba8d7aa8eb40c3161e0f635103
Shin Seokjin <ssj9685@naver.com>
2023-02-13 03:05:45 +0900

    update:static build

---
+++ build/_app/immutable/chunks/3-44ff0c1d.js
@@ -0,0 +1,6 @@
+import { _ } from "./_page-cbe3748d.js";
+import { default as default2 } from "../components/pages/about/_page.svelte-f31c9606.js";
+export {
+  default2 as component,
+  _ as universal
+};
---
+++ build/_app/immutable/chunks/5-9483b414.js
@@ -0,0 +1,6 @@
+import { _ } from "./_page-34b026b6.js";
+import { default as default2 } from "../components/pages/sverdle/how-to-play/_page.svelte-fb619894.js";
+export {
+  default2 as component,
+  _ as universal
+};
